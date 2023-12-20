8e46ebea59e08c174cde2d29a0ca7fe75ba4608e
SubhagyaBorus <135357408+SubhagyaBorus@users.noreply.github.com>
2023-12-20 15:02:14 +0530

    update

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
-//REST API demo in Node.js
-var express = require('express'); // requre the express framework
+var express = require('express');
+var cors = require('cors'); // Require the CORS middleware
 var app = express();
-var fs = require('fs'); //require file system object
+var fs = require('fs');
+
+// Use CORS middleware for all routes
+app.use(cors());
 
 // Endpoint to Get a list of users
 app.get('/getUsers', function(req, res){
     fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err, data){
         console.log(data);
-        res.end(data); // you can also use res.send()
+        res.end(data);
     });
-})
+});
+
 app.get('/getProducts', function(req, res){
     fs.readFile(__dirname + "/" + "products.json", 'utf8', function(err, data){
- 
-        res.end(data); // you can also use res.send()
+        res.end(data);
     });
-})
+});
 
 // Create a server to listen at port 8080
 var server = app.listen(8080, function(){
-    var host = server.address().address
-    var port = server.address().port
-    console.log("REST API demo app listening at http://%s:%s", host, port)
-})
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("REST API demo app listening at http://%s:%s", host, port);
+});
